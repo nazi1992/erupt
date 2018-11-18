83e62aa787afd057c20e1d24e1c4e453442f6812
xiongfeng <18228302169@163.com>
2018-11-18 10:26:44 +0800

    annoation

---
+++ src/main/java/com/example/demo/EruptApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EruptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EruptApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/demo/annoation/NotRecommend.java
@@ -0,0 +1,21 @@
+package com.example.demo.annoation;
+
+/**
+ * Created by Administrator on 2018/11/18 0018.
+ */
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**(
+ * 添加一些自己的注解
+ * 该注解主要是标识不推荐的类或写法
+ */
+//注解的目标,一般定义注解类，会有如下两个注解说明，
+@Target(ElementType.ANNOTATION_TYPE.TYPE)
+@Retention(RetentionPolicy.SOURCE)//注解存在的范围，该注解表示在编译期存在，因为该类只是为了标识而已
+public @interface NotRecommend {
+    String value() default "";
+}
